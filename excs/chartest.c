647c1bf6fb1fba4f85ee968368acd3bcc81e215e
Fernando Gigliotti <100796054+Nerfandao@users.noreply.github.com>
2022-08-24 20:57:18 -0300

    1st Commit

---
+++ excs/chartest.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char c1 = 'a';
+    char c2 = 115;
+
+    printf("%c\n",c1);
+    printf("%c\n",c2);
+    printf("%c\n",c1 + c2);
+
+    printf("%d\n",c1);
+    printf("%d\n",c2);
+    printf("%d\n",c1 + c2);
+
+    return 0;
+}

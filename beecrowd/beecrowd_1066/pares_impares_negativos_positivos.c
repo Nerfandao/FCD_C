90392e8c03a5aae37331f22e8866e15789dfbc0b
Fernando Gigliotti <100796054+Nerfandao@users.noreply.github.com>
2022-08-31 21:46:55 -0300

    BC 1066

---
+++ beecrowd/beecrowd_1066/pares_impares_negativos_positivos.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int A,B,C,D,E, PAR, IMPAR, POSITIVO, NEGATIVO;
+    PAR = 0;
+    IMPAR = 1;
+    POSITIVO = > 0;
+    NEGATIVO = < 0;
+    scanf("%d %d %d %d %d",&A,&B,&C,&D,&E);
+    if (A%2==0) {
+
+    }
+    return 0;
+}

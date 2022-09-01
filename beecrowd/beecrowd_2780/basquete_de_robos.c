89ba98982bfebd61f80f38085cd8efeaac63cd1b
Fernando Gigliotti <100796054+Nerfandao@users.noreply.github.com>
2022-08-31 21:22:04 -0300

    att 2780

--- beecrowd/beecrowd_2780/basquete_de_robos.c
+++ beecrowd/beecrowd_2780/basquete_de_robos.c
@@ -7,15 +7,15 @@ int main() {
     scanf("%d",&D);
 
     if (D <= 800){
-        printf("1pts\n");
+        printf("1\n");
     }
     else
         if ((800 < D) && (D <= 1400)){
-        printf("2pts\n");
+        printf("2\n");
     }
     else
         if ((1400 < D) && (D <= 2000)){
-        printf("3pts\n");
+        printf("3\n");
     }
 
 return 0;

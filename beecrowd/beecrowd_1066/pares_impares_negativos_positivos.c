6eb8f1f4aca57d854e6034e08034c10a58adcc9e
Fernando Gigliotti <pucca.bruzil@hotmail.com.br>
2022-09-02 11:37:18 -0300

    bc

--- beecrowd/beecrowd_1066/pares_impares_negativos_positivos.c
+++ beecrowd/beecrowd_1066/pares_impares_negativos_positivos.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
-
+ 
 int main() {
-    int A,B,C,D,E, PAR, IMPAR, POSITIVO, NEGATIVO;
-    PAR = 0;
-    IMPAR = 1;
-    POSITIVO = > 0;
-    NEGATIVO = < 0;
-    scanf("%d %d %d %d %d",&A,&B,&C,&D,&E);
-    if (A%2==0) {
-
+    int numero, par, impar, positivo, negativo;
+    scanf("%d %d %d %d %d",&numero);
+    if (numero%2==0){
+        par;
     }
+    if (numero%2==1){
+        impar;
+    }
+    if (0<numero){
+        positivo;
+    }
+    if (0>numero){
+        negativo;
+    }
+    printf("%d valor(es) par(es)\n",par);
+    printf("%d valor(es) impar(es)\n",impar);
+    printf("%d valor(es) positivo(s)\n",positivo);
+    printf("%d valor(es) negativo(s)\n",negativo);
     return 0;
 }
